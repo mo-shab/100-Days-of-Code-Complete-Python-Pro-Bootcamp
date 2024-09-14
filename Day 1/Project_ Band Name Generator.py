65aa2845f8513379a83767378462bd1e7f810cf3
Mohammed <mrcloud.med@gmail.com>
2024-09-14 21:51:17 +0000

    Day 1

---
+++ Day 1/Project_ Band Name Generator.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Band Name Generator"""
+
+print("Welcom to my Band Name Generator")
+
+city_name = input("What is the name of the city you grew up in?\n")
+
+pet_name = input("What is the name of the a pet?\n")
+
+print(f"Your Band Name is {city_name}{pet_name}!")
